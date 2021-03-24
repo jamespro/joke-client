55e42651333f24fbaadd8c775e0ed6e52246a5ee
James Prickitt <jwprickitt@gmail.com>
2021-03-24 11:56:17 -0400

    update api connection string

--- js/main.js
+++ js/main.js
@@ -6,7 +6,19 @@ const answer2 = document.getElementById('answer2')
 
 async function getJoke() {
     try {
-        const res = await fetch('http://localhost:8000/api/joke/'+jokeID.value)
+        //TODO this needs to check what server you're on,
+        //and/or have a way for you to pick which server
+        //const res = await fetch('http://localhost:8000/api/joke/'+jokeID.value)
+        let localdev = false
+        let http_protocol = 'https://'
+        let server_name = 'joke-api-jamespro.herokuapp.com'
+        if (localdev) {
+            //TODO why don't these get defined here? syntax?
+            let http_protocol = 'http://'
+            let server_name = 'localhost:8000'
+        }
+        let baseurl = http_protocol+server_name+'/api/joke/'
+        const res = await fetch(baseurl+jokeID.value)
         const data = await res.json()
         console.log(data)
         question.innerText = data.question
